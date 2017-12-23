1b99a34757c8141ad4ff1ef43d8190363f396cef
opaismoe <wiebe.buursink@gmail.com>
2017-12-23 21:03:03 +0100

    Fixxed seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,71 @@
 Artist.destroy_all
 Song.destroy_all
 
-artists = []
-60.times do
-  artists << Artist.create!(
-    name:   Faker::Name.name,
-    genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+artist1 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+artist2 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+artist3 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+artist4 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+artist5 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+artist6 = Artist.create! ({
+  name:   Faker::Name.name,
+  genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+})
+
+
+songs_artist1 = []
+15.times do
+  songs_artist1 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist1,
   )
 end
-
-songs = []
-artists.each do |artist|
-  songs << Song.create!(
-    name:   Faker::Name.name,
-    artist: artist,
+songs_artist2 = []
+15.times do
+  songs_artist2 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist2,
+  )
+end
+songs_artist3 = []
+15.times do
+  songs_artist3 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist3,
+  )
+end
+songs_artist4 = []
+15.times do
+  songs_artist4 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist4,
+  )
+end
+songs_artist5 = []
+15.times do
+  songs_artist5 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist5,
+  )
+end
+songs_artist6 = []
+15.times do
+  songs_artist6 << Song.create!(
+    name: Faker::Name.name,
+    artist: artist6,
   )
 end
