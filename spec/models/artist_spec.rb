68f492114fe142d95853bfc0efb346e722e809ad
opaismoe <wiebe.buursink@gmail.com>
2017-12-23 19:22:33 +0100

    Add test for validation and associations

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Check attributes" do
+
+    it "Artist has a name" do
+      artist = Artist.new(name: "Arthur dent")
+      expect(artist.name).to include(artist.name)
+    end
+
+    it "Artist has a genre" do
+      artist = Artist.new(genre: "LOUD")
+      expect(artist.genre).to include(artist.genre)
+    end
+  end
+
+  describe "Check validations" do
+    let!(:artist) {create :artist, name: nil, genre: nil}
+
+    it "is invalid without a name" do
+      artist.valid?
+      expect(artist.errors).to have_key(:name)
+    end
+
+    it "is invalid without a genre" do
+      artist.valid?
+      expect(artist.errors).to have_key(:genre)
+    end
+  end
+
+  describe "Association with songs" do
+    let!(:artist) { create :artist }
+    let!(:song) { create :song, user: artist}
+
+    it "Artist has an song" do
+      expect(artist.songs).to include(song)
+    end
+  end
 end
