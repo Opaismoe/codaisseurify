0a8510a3d797a1d56757451681f240f74f03e857
opaismoe <wiebe.buursink@gmail.com>
2017-12-23 20:07:30 +0100

    Add seed file, Fixxed dependent destroy in artist model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.destroy_all
+Song.destroy_all
+
+artists = []
+60.times do
+  artists << Artist.create!(
+    name:   Faker::Name.name,
+    genre:  Faker::HitchhikersGuideToTheGalaxy.planet,
+  )
+end
+
+songs = []
+artists.each do |artist|
+  songs << Song.create!(
+    name:   Faker::Name.name,
+    artist: artist,
+  )
+end
