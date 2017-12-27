8144b7fd69cc4091868299e9093c7dd89d89d5cc
opaismoe <wiebe.buursink@gmail.com>
2017-12-27 12:17:51 +0100

    Fix import error in application.scss, and fix routes file for Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "artists#index"
 
-  resources :artists, only: [:show, :index, :destroy] do
-    resources :songs, only: [:create, :destroy]
+  resources :artists do
+    resources :songs
   end
 
 end
