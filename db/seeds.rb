cbe562d4a02853d65ec818e1a0b97a6964a23efb
Wiebe Buursink <wiebe.buursink@gmail.com>
2018-02-14 23:13:55 +0100

    Removed photo's from seed, fix cloudinary/carrierwave

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,10 +70,12 @@ songs_artist6 = []
   )
 end
 
-photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/david-bowie.jpg", artist: artist1)
-photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/homeimage.jpg", artist: artist2)
-photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/33273-optimized_5984f5e408e0c.jpg", artist: artist3)
 
-photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/bert_jansch_1317896252_crop_550x344.jpg", artist: artist4)
-photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/oEwWZE1Y_400x400.jpg", artist: artist5)
-photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/o-GUCCI-MANE-facebook.jpg", artist: artist6)
+# TODO cloudinary / carrierwave
+# photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/david-bowie.jpg", artist: artist1)
+# photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/homeimage.jpg", artist: artist2)
+# photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/33273-optimized_5984f5e408e0c.jpg", artist: artist3)
+#
+# photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/bert_jansch_1317896252_crop_550x344.jpg", artist: artist4)
+# photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/oEwWZE1Y_400x400.jpg", artist: artist5)
+# photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/opaismoe/image/upload/v1514066658/codaisseurify/o-GUCCI-MANE-facebook.jpg", artist: artist6)
